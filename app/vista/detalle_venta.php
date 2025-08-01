83f66aa61c2c4e994c569611c5b5656c87ab9e8d
Alejandra Moreno <ale.moreno200502@gmail.com>
2025-08-01 09:22:03 -0500

    Update detalle_venta.php
    modificado

--- app/vista/detalle_venta.php
+++ app/vista/detalle_venta.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <title>Detalle de Venta #<?= $venta['id'] ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/tienda/assets/css/estilos.css?v=1.1">
 </head>
 <body class="bg-light">
 <div class="container my-5">
